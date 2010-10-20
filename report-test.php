cfd178148759c9d8cf2d46e2efe0b55e9df2eceb
vlad <vlad@admin.(none)>
2010-10-20 19:32:13 +0400

    report-test

--- report-test.php
+++ report-test.php
@@ -17,82 +17,129 @@ require_once('lib/query_gen.php');
 
 $page=new dom_root_print;
 $page->title='Тестовый отчет';
-
-function printhead($exstyle)
-{
-	global $encoding;
-	print "<html><head><title>Тестовый отчет</title>";
-	//print "<link rel=\"stylesheet\" href=\"default.css\" type=\"text/css\">";
-	print "<style type='text/css'>";
-	foreach($exstyle as $si=>$se)//selector
-	{
-		 print $si." {\n";
-		 foreach($se as $ai=>$ae)//attribute
-		 	print $ai.' : '.$ae." ;\n";
-		 print "}\n";
-	}
-	print "@media print {\n";
-	foreach($exstyle as $si=>$se)//selector
-	{
-		 print $si." {\n";
-		 foreach($se as $ai=>$ae)//attribute
-		 	print $ai.' : '.$ae." ;\n";
-		 print "}\n";
-	}
-	print "}\n";
-	print "</style>";
-	print "<meta http-equiv=content-type content=\"text/html; charset=UTF-8\"></head><body>\n";
-};
+$page->scripts[]='/js/core.js';
+$page->scripts[]='/js/commoncontrols.js';
 
 
-$exstyle['*']=Array(
+$page->exstyle['*']=Array(
 'margin'=>'0mm',
 'padding'=>'0mm'
 );	
+$page->exstyle['div']=Array(
+'font-size'=>'5mm',
+'padding'=>'0mm'
+);	
 
-$exstyle['.pb']=Array(
-'page-break-before'=>'avoid',
-'page-break-after'=>'always',
-//'margin'=>'auto',
-'height'=>'30mm',
-'padding-top'=>'2mm',
-'margin-left'=>'-1.8mm',
-'width'=>'57mm',
-//'border'=>' 1px solid black',
-'overflow'=>'hidden'
-);	
-$exstyle['.pb>div.code']=Array(
-'width'=>'56mm',
-'height'=>'18mm',
-'text-align'=>'left',
-'margin'=>'auto',
-'padding'=>'0mm',
-//'outline'=>'1px solid black',
-'font-family'=>'EanGnivc',
-//'display'=>'none',
-'font-size'=>'16mm'
-);	
-$exstyle['.pb>div.text']=Array(
-'width'=>'54mm',
-'height'=>'10mm',
-'margin'=>'auto',
-'vertical-align'=>'middle'
-);
-$exstyle['.pb>div.text div']=Array(
-'font-family'=>'helvetica',
-'font-size'=>'2.8mm',
-'line-height'=>'3.5mm',
-'text-align'=>'center',
-//'outline'=>'1px solid black',
-'word-spacing'=>'0mm',
-'letter-spacing'=>'0mm',
-'overflow'=>'hidden'
-);	
-$exstyle['@page']=Array(
+$page->exstyle['@page']=Array(
 'margin'=>'0mm',
 'padding'=>'0mm'
 //'size'=>'57mm 31mm'
 //,'size'=>'landscape'
 );
+
+class qres extends dom_any
+{
+	function __construct()
+	{
+		parent::__construct('table');
+		$this->css_style['padding']='0px';
+		$this->css_style['margin']='0px';
+		$this->css_style['border']='0px';
+		$this->css_style['border-collapse']='collapse';
+		$this->row=new dom_tr;
+		$this->cell=new dom_td;
+		$this->cell->css_style['padding']='0px';
+		$this->cell->css_style['margin']='0px';
+		$this->cell->css_style['border']='0px';
+		$this->data=new dom_statictext;
+		unset($this->row->id);
+		unset($this->cell->id);
+		$this->append_child($this->row);
+		$this->row->append_child($this->cell);
+		$this->cell->append_child($this->data);
+	}
+	
+	function html_inner()
+	{
+		global $sql;
+		$qc=$this->qg->result();
+		$res=$sql->query($qc);
+		while($row=$sql->fetchn($res))
+		{
+			$this->row->html_head();
+			foreach($row as $v)
+			{
+				$this->data->text=$v;
+				$this->cell->html();
+			}
+			$this->row->html_tail();
+		}
+	}
+}
+
+
+$div=new dom_div;
+$div->css_style['width']='190mm';
+$q=new qres;
+$qid=$q->id_gen();
+$q->qg=new query_gen_ext('select');
+$q->qg->from->exprs[]=new sql_column(NULL,'barcodes_raw',NULL,NULL);
+$q->qg->what->exprs[]=new sql_column(NULL,NULL,'id','id');
+$q->qg->what->exprs[]=new sql_column(NULL,NULL,'name','name');
+$q->qg->what->exprs[]=new sql_column(NULL,NULL,'code','code');
+$q->qg->lim_count=500;
+
+$div->append_child($q);
+$page->append_child($div);
+
+$page->endscripts[]=
+"
+window.onload=function()
+{
+	var d=document.createElement('div');
+	d.appendChild(document.createTextNode(' '));
+	d.style.width='1000mm';
+	d.style.height='1000mm';
+	document.body.appendChild(d);
+	var r=document.createTextNode('1000mm is '+d.offsetWidth.toString()+'px');
+	var px_h=Math.round((d.offsetWidth*260)/1000);
+	var px_w=Math.round((d.offsetWidth*200)/1000);
+	document.body.appendChild(r);
+	document.body.removeChild(d);
+	//alert(px280+','+px200);
+	var tbl=\$i('".$qid."');
+	tbl=tbl.firstChild;
+	var cc=tbl.firstChild;
+	var ah=0;
+	while(cc!=null)
+	{
+		if(ah>=px_h)
+		{
+			ah=0;
+			cc.style.pageBreakBefore='always';
+			cc.style.backgroundColor='red';
+		}
+		ah+=cc.offsetHeight;
+		cc.title=ah.toString()+' , '+px_h;
+		cc=cc.nextSibling;
+	}
+	
+};
+";
+
+
+
+
+
+
+
+
+
+
+
+
+
+$page->html();
+
 
 ?>
